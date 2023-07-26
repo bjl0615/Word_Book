a165e438a3f2378017e5bc05dd13a4700adfe5f9
admin <bjl0615@gmail.com>
2023-07-26 18:06:32 +0900

    Word_Book UI_2

--- app/src/main/java/com/example/word_book/AddActivity.kt
+++ app/src/main/java/com/example/word_book/AddActivity.kt
@@ -2,10 +2,36 @@ package com.example.word_book
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.word_book.databinding.ActivityAddBinding
+import com.google.android.material.chip.Chip
 
 class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val types = listOf(
+            "명사", "동사", "대명사", "형용사", "부사", "김틴시", "전치사", "접속사"
+        )
+        binding.typeChipGroup.apply {
+            types.forEach { text ->
+                addView(createChip(text))
+            }
+        }
+    }
+
+    private fun createChip(text: String) : Chip{
+        return Chip(this).apply {
+            setText(text)
+            isCheckable = true
+            isClickable
+        }
     }
 }
